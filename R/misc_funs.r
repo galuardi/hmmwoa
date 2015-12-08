4b174ccc9959721d56ea8f127738e2ca994b5bac
camrinbraun <camrin.braun@gmail.com>
2015-12-08 15:12:48 -0500

    added resolution to setup.grid because high resolution grid is taking too long in make.kern

--- R/misc_funs.r
+++ R/misc_funs.r
@@ -1,6 +1,9 @@
-setup.grid <- function(locations){
+setup.grid <- function(locations, res){
   ## Setup the discrete spatial grid for the HMM
 #' @param locations is dataframe of -Locations from WC psat tag
+#' @param res is character indicating resolution of grid. 'hycom' is
+#'        .08 to match hycom reanalysis res. 'quarter' and 'one' are
+#'        .25 and 1 deg, respectively.
   
   T <- length(locations$Longitude)
   
@@ -10,7 +13,14 @@ setup.grid <- function(locations){
   lx <- 0.1 * (al - il)
   lonl <- il - lx
   lonu <- al + lx
-  lo.out <- 25/2 * (lonu - lonl)
+  
+  if(res == 'hycom'){
+    lo.out <- 25/2 * (lonu - lonl)
+  } else if(res == 'quarter'){
+    lo.out <- 4 * (lonu - lonl)
+  } else if(res == 'one'){
+    lo.out <- 1 * (lonu - lonl)
+  }
   
   # Find latitude extents
   ila <- floor(min(locations$Latitude))
@@ -18,7 +28,14 @@ setup.grid <- function(locations){
   ly <- 0.1 * (ala - ila)
   latl <- ila - ly
   latu <- ala + ly
-  la.out <- (25/2) * (latu - latl)
+ 
+   if(res == 'hycom'){
+    la.out <- 25/2 * (latu - latl) # 0.08 deg
+  } else if(res == 'quarter'){
+    la.out <- 4 * (latu - latl) # .25 deg
+  } else if(res == 'one'){
+    la.out <- 1 * (latu - latl) # 1 deg
+  }
   
   #  latvec <- seq(0,90)
   #  lats <- rep(0,T)
