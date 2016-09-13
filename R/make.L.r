edf6dcb6b4b3f0765c751e9f4a508123ed82acf0
camrinbraun <camrin.braun@gmail.com>
2016-09-13 12:04:40 -0400

    put all the array/raster manipulation and algebra into a function

--- R/make.L.r
+++ R/make.L.r
@@ -11,80 +11,113 @@
 #' @examples
 #' none
 
+make.L <- function(L1, L2 = NULL, L3 = NULL, L.mle.res, plot = TRUE){
 
-make.L <- function(L.res, idx = c(1,2,3,4), plot = TRUE){
-  # where 1==pdt, 2==ohc, 3==locs, 4==sst
-  
-  # MAKE AN ARRAY OF ZEROS
-  Lmat = L.res[[1]][[1]] * 0
-  
-  # RASTER TO ARRAY
-  L.res <- lapply(L.res[[1]], FUN=raster::as.array)
-  
-  # GET RID OF NA VALUES - not working
-  L.res[is.na(L.res)] = 0
-  
-  napdtidx = apply(L.res$L.pdt, 3, sum, na.rm=T) != 0
-  
-  # need to sum rows and cols at each layer as in apply(x, 3, sum) but for a raster
-  pdtidx <- sum(L.pdt, na.rm=T) != 0
-  
-  if(length(idx) == 1){
+  if(is.null(L2) & is.null(L3)){
+    L <- L1
     
-    # only one input then there's nothing to do here
-    L.res[[1]][idx]
+    # GET RID OF NA VALUES
+    L[is.na(L)] <- 0
     
-  } else if(length(idx) == 2){
+    # ALL CELLS IN A LIKELIHOOD SURFACE == 0?
+    naLidx = cellStats(L, sum, na.rm=T) != 0
     
-    # two inputs get added/multiplied together
-    Lmat[,,idx1] = L.locs[,,idx1] + L.sst[,,idx1] # when only 1 has data
-    Lmat[,,idx2] = L.sst[,,idx2] * L.locs[,,idx2] # when both have data
+    L <- L[naLidx]
     
-  } else if(length(idx) == 3){
+  } else if(!is.null(L2) & is.null(L3)){
+    # then there were 2
     
-    Lmat[,,idx1] = L.locs[,,idx1] + L.sst[,,idx1] + L.ohc[,,idx1] # when only 1 has data
-    Lmat[,,idx3] = L.locs[,,idx3] * L.sst[,,idx3] * L.ohc[,,idx3] # when all have data
+    # MAKE AN ARRAY OF ZEROS
+    L <- L1 * 0
     
-    # USE THE INDICES TO POPULATE L
-    for(b in which(idx2)){
-      if(nasstidx[b] & nalocidx[b]){
-        Lmat[,,b] = L.sst[,,b] * L.locs[,,b]
-      } else if(nasstidx[b] & naohcidx[b]){
-        Lmat[,,b] = L.sst[,,b] * L.ohc[,,b]
-      } else if(nalocidx[b] & naohcidx[b]){
-        Lmat[,,b] = L.locs[,,b] * L.ohc[,,b]
+    # GET RID OF NA VALUES
+    L1[is.na(L1)] <- 0
+    L2[is.na(L2)] <- 0
+    
+    # ALL CELLS IN A LIKELIHOOD SURFACE == 0?
+    naL1idx = cellStats(L1, sum, na.rm=T) != 0
+    naL2idx = cellStats(L2, sum, na.rm=T) != 0
+    
+    # WHERE BOTH ARE ZEROS. THESE WILL BE INTERPOLTED IN THE FILTER
+    naLidx = naL1idx + naL2idx
+    
+    # where naLidx==0, both likelihoods are zero
+    #       naLidx==1, one has data
+    #       naLidx==2, both have data
+    idx1 = which(naLidx == 1)
+    idx2 = which(naLidx == 2)
+
+    
+    # INPUTS GET ADDED/MULTIP TOGETHER FOR FINAL L
+    # THESE DONT WORK
+    #L[[idx1]] <- L1[[idx1]] + L2[[idx1]] 
+    #L[[idx1]] <- overlay(L1[[idx1]], L2[[idx1]], fun=function(a,b){a + b})
+    
+    # BUT THIS DOES
+    for(ii in idx1){
+      L[[ii]] = L1[[ii]] + L2[[ii]] # when only 1 has data
+    }
+    
+    for(ii in idx2){
+      L[[ii]] = L1[[ii]] * L2[[ii]] # when both have data
+    }
+
+    
+
+  } else if(!is.null(L2) & !is.null(L3)){
+    # then there were 3
+    
+    # MAKE AN ARRAY OF ZEROS
+    L <- L1 * 0
+    
+    # GET RID OF NA VALUES
+    L1[is.na(L1)] <- 0
+    L2[is.na(L2)] <- 0
+    L3[is.na(L3)] <- 0
+    
+    # ALL CELLS IN A LIKELIHOOD SURFACE == 0?
+    naL1idx = cellStats(L1, sum, na.rm=T) != 0
+    naL2idx = cellStats(L2, sum, na.rm=T) != 0
+    naL3idx = cellStats(L3, sum, na.rm=T) != 0
+    
+    # WHERE BOTH ARE ZEROS. THESE WILL BE INTERPOLTED IN THE FILTER
+    naLidx = naL1idx + naL2idx + naL3idx
+    
+    # where naLidx==0, both likelihoods are zero
+    #       naLidx==1, one has data
+    #       naLidx==2, both have data
+    idx1 = which(naLidx == 1)
+    idx2 = which(naLidx == 2)
+    idx3 = which(naLidx == 3)
+    
+    # COMBINING LIKELIHOODS
+    for(ii in idx1){
+      L[[ii]] = L1[[ii]] + L2[[ii]] + L3[[ii]]# when only 1 has data
+    }
+    
+    for(ii in idx3){
+      L[[ii]] = L1[[ii]] * L2[[ii]] * L3[[ii]] # when all have data
+    }
+    
+    for(ii in idx2){
+      if(naL1idx[ii] & naL2idx[ii]){
+        L[[ii]] <- L1[[ii]] * L2[[ii]]
+      } else if(naL1idx[ii] & naL3idx[ii]){
+        L[[ii]] <- L1[[ii]] * L3[[ii]]
+      } else if(naL2idx[ii] & naL3idx[ii]){
+        L[[ii]] <- L2[[ii]] * L3[[ii]]
       }
     }
     
-  } else if(length(idx) == 4){
-    
-    stop('This many input indices is not yet functional. Make sure you are not trying to use both pdt and ohc inputs.')
-    
-  }
-  
-  # MAKE A LIST OF LIKELIHOOD TO SET RASTER EXTENT
-  # define projection
-  crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
-  list.pdt <- list(x = lon, y = lat, z = Lmat)
-  ex <- raster::extent(list.pdt)
-  
-  # MAKE A RASTER OUT OF IT
-  T <- dim(Lmat)[3]
-  for(i in 1:T){
-    L.i <- raster::raster(Lmat[,,i], xmn=ex[1], xmx=ex[2], ymn=ex[3], ymx=ex[4], crs)
-    if(i==1) L <- L.i else L <- stack(L, L.i)
   }
   
   # CREATE A MORE COARSE RASTER FOR PARAMETER ESTIMATION LATER
-  L.mle <- raster::resample(L, L.mle.res)
-  
-  # MAKE BOTH RASTERS (COARSE AND FINE RES L's) INTO AN ARRAY
-  #L <- aperm(raster::as.array(raster::flip(L, direction = 'y')), c(3, 2, 1))
-  #L.mle <- aperm(raster::as.array(raster::flip(L.mle, direction = 'y')), c(3, 2, 1))
-  
+  L.mle <- raster::resample(L, L.mle.res$L.locs)
+
   if(plot){
     # CHECK THAT IT WORKED OK
-    plot(L[[1]])
+    require(fields)
+    image.plot(L[1,,])
     plot(countriesLow,add=T)
     
   }
@@ -93,109 +126,16 @@ make.L <- function(L.res, idx = c(1,2,3,4), plot = TRUE){
   # MAKE ALL NA'S VERY TINY FOR THE CONVOLUTION
   # the previous steps may have taken care of this...
   #----------------------------------------------------------------------------------#
-  L[L == 0] = 1e-15
-  L[is.na(L)] = 1e-15
-  L.mle[L.mle == 0] = 1e-15
-  L.mle[is.na(L.mle)] = 1e-15
+  L[L == 0] <- 1e-15
+  L[is.na(L)] <- 1e-15
+  L.mle[L.mle == 0] <- 1e-15
+  L.mle[is.na(L.mle)] <- 1e-15
   
+  # MAKE BOTH RASTERS (COARSE AND FINE RES L's) INTO AN ARRAY
+  L <- aperm(raster::as.array(raster::flip(L, direction = 'y')), c(3, 2, 1))
+  L.mle <- aperm(raster::as.array(raster::flip(L.mle, direction = 'y')), c(3, 2, 1))
   
-  
+  return(list(L = L, L.mle = L.mle))
 }
 
 ## END
-
-# are all cells in a given likelihood surface == 0?
-#na1idx = lapply(L.res[[1]], FUN = (apply(L.res[[1]][[1]], 3, sum, na.rm=T) != 0))# does sum of likelihood surface
-#na2idx = apply(L.2, 3, sum, na.rm=T) != 0
-#na3idx = apply(L.3, 3, sum, na.rm=T) != 0
-#na4idx = apply(L.4, 3, sum, na.rm=T) != 0
-
-
-# HERE, WE CHOOSE WHICH L's TO USE
-# INDICATES WHICH L LAYERS, IF ANY, ARE ALL ZEROS FOR EACH DAY
-
-
-# WHERE BOTH ARE ZEROS. THESE WILL BE INTERPOLTED IN THE FILTER
-#naLidx = nalocidx + nasstidx + naohcidx
-
-# MAKE AN ARRAY OF ZEROS
-#Lmat = L.pdt * 0
-# where naLidx==0, both likelihoods are zero
-#       naLidx==1, one has data
-#       naLidx==2, both have data
-#idx1 = naLidx == 1
-#idx2 = naLidx == 2
-#idx3 = naLidx == 3
-
-#Lmat[,,idx1] = L.locs[,,idx1] + L.sst[,,idx1] + L.ohc[,,idx1] # when only 1 has data
-#Lmat[,,idx2] = L.sst[,,idx2] * L.locs[,,idx2] # when both have data
-#Lmat[,,idx3] = L.locs[,,idx3] * L.sst[,,idx3] * L.ohc[,,idx3] # when all have data
-
-# USE THE INDICES TO POPULATE L
-#for(b in which(idx2)){
-#  if(nasstidx[b] & nalocidx[b]){
-#    Lmat[,,b] = L.sst[,,b] * L.locs[,,b]
-#  } else if(nasstidx[b] & naohcidx[b]){
-#    Lmat[,,b] = L.sst[,,b] * L.ohc[,,b]
-#  } else if(nalocidx[b] & naohcidx[b]){
-#    Lmat[,,b] = L.locs[,,b] * L.ohc[,,b]
-#  }
-#}
-
-# MAKE A LIST OF LIKELIHOOD TO SET RASTER EXTENT
-# define projection
-#crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
-#list.pdt <- list(x = lon, y = lat, z = Lmat)
-#ex <- raster::extent(list.pdt)
-
-# MAKE A RASTER OUT OF IT
-#T <- dim(Lmat)[3]
-#for(i in 1:T){
-#  L.i <- raster::raster(Lmat[,,i], xmn=ex[1], xmx=ex[2], ymn=ex[3], ymx=ex[4], crs)
-#  if(i==1) L <- L.i else L <- stack(L, L.i)
-#}
-
-# CREATE A MORE COARSE RASTER FOR PARAMETER ESTIMATION LATER
-#L.mle <- raster::resample(L, L.mle.res)
-
-# MAKE BOTH RASTERS (COARSE AND FINE RES L's) INTO AN ARRAY
-#L <- aperm(raster::as.array(raster::flip(L, direction = 'y')), c(3, 2, 1))
-#L.mle <- aperm(raster::as.array(raster::flip(L.mle, direction = 'y')), c(3, 2, 1))
-
-#if(plot){
-#  # CHECK THAT IT WORKED OK
-#  lon <- g$lon[1,]
-#  lat <- g$lat[,1]
-#  fields::image.plot(lon, rev(lat), L[1,,])
-#  par(mfrow=c(2,1))
-#  fields::image.plot(lon, lat, L2[1,,])
-#  image.plot(L1[1,,])
-#  plot(countriesLow,add=T)
-#  
-#}
-
-#----------------------------------------------------------------------------------#
-# MAKE ALL NA'S VERY TINY FOR THE CONVOLUTION
-# the previous steps may have taken care of this...
-#----------------------------------------------------------------------------------#
-#L[L == 0] = 1e-15
-#L[is.na(L)] = 1e-15
-#L.mle[L.mle == 0] = 1e-15
-#L.mle[is.na(L.mle)] = 1e-15
-
-
-#*** end function
-
-# INDEX WHERE LIKELIHOODS ARE ZEROS.. FOR EACH L COMPONENT
-#L.1 = raster::as.array(L.res[[1]][[1]])
-#L.2 = raster::as.array(L.res[[1]][2])
-#L.3 = raster::as.array(L.res[[1]][3])
-#L.4 = raster::as.array(L.res[[1]][4])
-
-# turn NA to 0
-#L.1[is.na(L.1)] = 0
-#L.2[is.na(L.2)] = 0
-#L.3[is.na(L.3)] = 0 
-#L.4[is.na(L.4)] = 0
-
-cellStats yields sum of each layer in raster stack
