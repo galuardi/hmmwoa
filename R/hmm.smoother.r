ce7b069bd6398954a642d2a8c723d44753cf30e8
camrinbraun <camrin.braun@gmail.com>
2016-11-21 10:22:28 -0500

    may need to add this index to smooth output where t=1 is fixed/known

--- R/hmm.smoother.r
+++ R/hmm.smoother.r
@@ -21,13 +21,13 @@ hmm.smoother <- function(f, K1, K2, P, plot = TRUE){
   K2 <- imager::as.cimg(K2)
   
   smooth <- array(0, dim = dim(f$phi))
+  #smooth[,1,,] <- f$phi[,1,,]
   smooth[,T,,] <- f$phi[,T,,]
   
   for(t in T:2){
     RAT <- smooth[,t,,] / (f$pred[,t,,]+1e-15)
     #     Rp1 <- as.vector(K1 %*% as.vector(RAT[1,,]))
     #     Rp2 <- as.vector(K2 %*% as.vector(RAT[2,,]))
-    
     
     p1 <- imager::as.cimg(t(RAT[1,,]))
     Rp1 <- imager::convolve(p1, K1)
@@ -44,15 +44,18 @@ hmm.smoother <- function(f, K1, K2, P, plot = TRUE){
       image.plot(Rp2)
       #plot(countriesLow,add=T)
     }
-    
     post1 <- matrix(P[1,1] * Rp1 + P[1,2] * Rp2, row, col)
     post2 <- matrix(P[2,1] * Rp1 + P[2,2] * Rp2, row, col)
+
     post1 <- post1 * f$phi[1,t-1,,]
     post2 <- post2 * f$phi[2,t-1,,]
+
     fac <- sum(as.vector(post1)) + sum(as.vector(post2))
     smooth[1,t-1,,] <- post1 / fac
     smooth[2,t-1,,] <- post2 / fac
   }
+  
   smooth
+
 }
 
