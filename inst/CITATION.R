6537d286901d44546a97e4bf86d3f38cca39efba
camrinbraun <camrin.braun@gmail.com>
2016-12-01 11:24:27 -0500

    the start to a citation file but it is incomplete

---
+++ inst/CITATION.R
@@ -0,0 +1,19 @@
+citHeader("To cite HMMoce in publications use:")
+
+citEntry(entry = "Article",
+         title        = "HMMoce: an R package for improved analysis of marine animal movement data using hidden Markov models",
+         author       = personList(as.person("Camrin D Braun"),
+                                   as.person("Benjamin Galuardi"),
+                                   as.person("Simon R Thorrold")),
+         journal      = "Methods in Ecology and Evolution",
+         year         = "subm",
+         volume       = "x",
+         number       = "x",
+         pages        = "x",
+         url          = "x",
+         
+         textVersion  =
+           paste("Braun CD, Galuardi B, Thorrold ST (submitted).",
+                 "HMMoce: an R package for improved analysis of marine animal movement data using hidden Markov models",
+                 "Methods in Ecology and Evolution, x(x), x-x.")
+)
